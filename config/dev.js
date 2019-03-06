79e36e4ce77d8e2f09cbcf9d482d29276875e1ab
Thorsten Kober <info@flashdesignory.com>
2019-03-06 10:59:41 -0800

    initial commit

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID:
+    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
+  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
+  mongoURI: 'mongodb://testing:testing123@ds143132.mlab.com:43132/blog_dev',
+  cookieKey: '123123123'
+};
